ba684817fcb642f32edd63d4eb57eda9be54da66
RuslanWorkGit <lulka758@gmail.com>
2025-03-17 21:43:24 +0200

    Create empty project

---
+++ TestTaskAbz/TestTaskAbz/TestTaskAbzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskAbzApp.swift
+//  TestTaskAbz
+//
+//  Created by Ruslan Liulka on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskAbzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
